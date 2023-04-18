21adae43dc468306791638c54ffce57dc86a3ffa
SGabry17 <112784051+SGabry17@users.noreply.github.com>
2023-04-18 10:04:46 -0600

    Format

--- main.tf
+++ main.tf
@@ -14,30 +14,28 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default"{
+data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
 }
 
-
-
 module "blog_sg" {
-  source            = "terraform-aws-modules/security-group/aws"
-  version           = "4.17.2"
-  name              = "blog_new"
-  ingress_rule      = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_rule = ["0.0.0.0/0"]
-  egress_rule      = ["all-all"]
-  egress_cidr_rule = ["0.0.0.0/0"]
-  vpc_id            = data.aws_vpc.default.id
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+
+  vpc_id  = data.aws_vpc.default.id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
